5872abb464e3f221d9e2ef20db7f28b57b34ab94
K-Nithish-Kumar <nithish81812@gmail.com>
2026-06-18 20:08:41 +0530

    Adding BasicOperators

---
+++ Javascript-Fundamentals-Part-1/BasicOperators/script.js
@@ -0,0 +1,19 @@
+// Mathematical & Arithmetic operators -> + , - , * , /
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+
+// Assignmet operator
+let x, y;
+console.log(x = y = 10 + 28 + 30);
+
+// Comparision Operator -> (>, <, >=, <=)
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
