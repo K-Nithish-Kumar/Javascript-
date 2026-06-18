e7021538a1db9595c2467b3a8a1ac4c1b486408f
K-Nithish-Kumar <nithish81812@gmail.com>
2026-06-18 20:05:51 +0530

    Adding values and variables

---
+++ Javascript-Fundamentals-Part-1/Values_Variables/script.js
@@ -0,0 +1,14 @@
+// Values is a piece of Information For Example
+'Nithish' // -> this is an information
+30 // -> is an information
+
+// Variable -> is used to store values For Example
+let firstName = 'Nithish';
+let age = 20;
+
+// Variables are classified into 3 types let, const and var
+
+let lastName = 'kumar'; // let is used to store value, reassigning is applicable, block - scoped
+const birthYear = 20; // used to store value, reassigning is not applicable
+var skill = 'Programming'; // similar to let but its used in old version of javascript code, function-scoped
+
