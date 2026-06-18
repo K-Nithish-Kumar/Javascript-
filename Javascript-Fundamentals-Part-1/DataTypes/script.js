fbac9429faea7d41200925b25aaf7b8882b1407e
K-Nithish-Kumar <nithish81812@gmail.com>
2026-06-18 20:06:47 +0530

    Adding DataTypes

---
+++ Javascript-Fundamentals-Part-1/DataTypes/script.js
@@ -0,0 +1,47 @@
+// Data types in JavaScript
+
+// Primitives - 7 types
+/* 1.Number
+   2.String
+   3.Boolean
+   4.Undefined
+   5.null
+   6.symbol
+   7.BigInt
+*/
+
+// Number 
+let age = 20;
+console.log(age);
+
+// String 
+let firstName = 'Raju';
+console.log(firstName);
+
+// Boolean 
+let isAllowed = (age > 18);
+console.log(isAllowed);
+
+// Undefined 
+let currentJob;
+console.log(currentJob);
+
+// null 
+let lastJob = null;
+console.log(lastJob);
+
+// Symbol
+const id1 = Symbol("userId")
+const id2 = Symbol("userId")
+
+console.log(id1 == id2);
+
+// BigInt
+const bigInt1 = 9007199254740991n;
+console.log(bigInt1 + 1n);
+
+
+
+
+
+
