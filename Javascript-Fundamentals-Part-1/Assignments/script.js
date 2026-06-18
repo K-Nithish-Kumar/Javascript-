b02373ffb9faec2ec521221b8688e7e638d90f28
K-Nithish-Kumar <nithish81812@gmail.com>
2026-06-18 20:08:00 +0530

    Adding Assignments

---
+++ Javascript-Fundamentals-Part-1/Assignments/script.js
@@ -0,0 +1,12 @@
+/* Values and Variables
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console. */
+
+const country = 'India';
+const continent = 'Asia';
+const population = 1430;
+
+console.log(country);
+console.log(continent);
+console.log(population);
