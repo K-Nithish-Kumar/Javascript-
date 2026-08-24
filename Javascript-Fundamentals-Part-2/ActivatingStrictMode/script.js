fa4239f60115fa285b88721a4db216de24260205
K-Nithish-Kumar <nithish81812@gmail.com>
2026-08-24 20:11:05 +0530

    Adding activating of strict mode

---
+++ Javascript-Fundamentals-Part-2/ActivatingStrictMode/script.js
@@ -0,0 +1,11 @@
+// use strict is used to avoid to use forbid one and it indicates the error
+'use strict';
+
+let hasDriverLicence = false;
+const passTest = true;
+
+if(passTest) hasDriversLicence = true;
+if(hasDriverLicence) console.log('I can drive a car...🚗');
+
+// use strict as well as reserved some words for future use we can't use it as a variable
+const interface = 565;
