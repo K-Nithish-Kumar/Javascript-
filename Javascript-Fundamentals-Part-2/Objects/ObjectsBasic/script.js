6e6e4807ea428336d058d348c7478378c4d52f3c
K-Nithish-Kumar <nithish81812@gmail.com>
2026-09-01 21:29:44 +0530

    Adding object basics

---
+++ Javascript-Fundamentals-Part-2/Objects/ObjectsBasic/script.js
@@ -0,0 +1,9 @@
+// Object is used to store the data as a key-value pair
+// Syntax
+const nithish = {
+    firstName: "Nithish",
+    lastName: "kumar",
+    age: 2026 - 2006,
+    profession: "Full Stack Developer",
+    friends: ['Haresh', 'Hari', 'Dinesh', 'Nithish', 'Sathish', 'Jeyaram', 'Akash']
+}
