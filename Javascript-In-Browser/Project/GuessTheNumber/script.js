4a84ae4e297f0b03d26e7c551cc76928db0aacef
K-Nithish-Kumar <nithish81812@gmail.com>
2026-09-19 21:57:45 +0530

    Selecting the html elements

---
+++ Javascript-In-Browser/Project/GuessTheNumber/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// selecting elements
+console.log(document.querySelector('.message').textContent);
+
+// Changing elements text content
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 22;
+
+// Get the value of the input element
+document.querySelector('.guess').value = 13;
